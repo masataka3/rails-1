59e872cad38627cc4bae530a740fec12bfdcd465
masataka <hanayoriinko@gmail.com>
2020-04-20 20:24:02 +0900

    start 3-8

--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '75bb76b8ef9feb7f5efa3186c66a5f555772f63db0bc22424d2c8b63c8c3847219d75bfea02ab5cf5adb60d21e68d67a27ff1f0156b7cd420f95208b1c9d001e'
